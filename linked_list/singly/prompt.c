2e3e19af1d17c6517847d346800fa3a92f8b242d
mosesgitonga <infosec947@gmail.com>
2023-04-24 13:46:53 +0300

    sum of input

---
+++ linked_list/singly/prompt.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+
+	struct node{
+	int num;
+	struct node *next;
+	};
+	struct node *head = NULL, *newnode = NULL, *temp = NULL;
+
+	int digit, sum = 0;
+
+	printf("enter a digit ");
+	
+	
+	while (scanf("%d", &digit) == 1 && getchar() != '\n')
+	{
+		newnode = (struct node *)malloc(sizeof(struct node));
+		newnode->num = digit;
+		newnode->next = NULL;
+
+		if (head == NULL)
+		{
+			head = newnode;
+			temp = newnode;
+		}
+		else
+		{
+			temp->next = newnode;
+			temp = newnode;
+		}
+	}
+	temp = head;
+	while (temp != NULL)
+	{
+		printf("sum of %d\n ", temp->num);
+		sum += temp->num;
+		temp = temp->next;
+	}
+	printf(" is %d\n", sum);
+	temp = head;
+	
+	while (temp != NULL)
+	{
+		head = temp->next;
+		free(temp);
+		temp = head;
+	}
+	
+	return (0);
+}
